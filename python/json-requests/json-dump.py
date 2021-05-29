edd58fa4eee61f91324e3ce462136efad5defe50
Anas <anas.ergeshov.98@gmail.com>
2021-05-29 21:23:44 +0600

    changes

--- python/json-requests/json-dump.py
+++ python/json-requests/json-dump.py
@@ -7,7 +7,7 @@ student = {
 }
 
 # Открываю файл для записи
-with open('dump-example.json', 'w') as file:
+with open('dump-ex-2.json', 'w') as file:
     # json.dump для записи в json формат
     json.dump(student, file)
 
--- python/json-requests/json-load.py
+++ python/json-requests/json-load.py
@@ -1,8 +1,17 @@
-import json  # Модуль для работы с json
+import json
 
+from requests.api import request  # Модуль для работы с json
+
+res = {}
 # Открываю json файл для получения данных
 with open('load-example.json', 'r') as f:
     # json.load для загрузки json данных в dictionary
     res = json.load(f)
+    res['language'] = 'Python'
     print(res)
+
+with open('load-example.json', 'w') as f:
+    json.dump(res, f)
+
+
  
